1dee1a835d03d71ab85fbadd0ca56be6b71b2f9d
Matt Travi <programmer@travi.org>
2021-11-30 11:49:40 -0600

    refactor(config-updater): extracted logic into more meaningful functions

--- src/lift/config.js
+++ src/lift/config.js
@@ -13,8 +13,39 @@ function legacyConfigExists(v3Exists, v4ConfigExists) {
   return v3Exists || v4ConfigExists;
 }
 
-function configFormatShouldBeUpdated(version, v3Exists, huskyV4ConfigExists) {
-  return semver.gte(version, '5.0.0') && legacyConfigExists(v3Exists, huskyV4ConfigExists);
+async function configFormatShouldBeUpdated(projectRoot, v3Exists, huskyV4ConfigExists) {
+  const {stdout: huskyVersionDetails} = await execa('npm', ['ls', 'husky', '--json']);
+
+  const {dependencies} = JSON.parse(huskyVersionDetails);
+
+  return semver.gte(dependencies.husky.version, '5.0.0') && legacyConfigExists(v3Exists, huskyV4ConfigExists);
+}
+
+async function updateOutdatedConfigFormat(
+  projectRoot,
+  packageManager,
+  v3Exists,
+  packageFilePath,
+  packageContents,
+  otherScripts,
+  huskyV4ConfigExists
+) {
+  const outdatedConfigMessage = 'Husky configuration is outdated for the installed Husky version';
+
+  info(outdatedConfigMessage, {level: 'secondary'});
+
+  const [results] = await Promise.all([
+    scaffold({projectRoot, packageManager}),
+    v3Exists
+      ? fs.writeFile(packageFilePath, JSON.stringify({
+        ...packageContents,
+        scripts: {...otherScripts}
+      }))
+      : Promise.resolve(),
+    huskyV4ConfigExists ? fs.unlink(`${projectRoot}/.huskyrc.json`) : Promise.resolve()
+  ]);
+
+  return results;
 }
 
 export async function updateConfigToMatchInstalledVersion({projectRoot, packageManager}) {
@@ -24,27 +55,16 @@ export async function updateConfigToMatchInstalledVersion({projectRoot, packageM
   const v3Exists = v3ConfigExists(precommit, commitmsg);
   const huskyV4ConfigExists = await fileExists(`${projectRoot}/.huskyrc.json`);
 
-  const {stdout: huskyVersionDetails} = await execa('npm', ['ls', 'husky', '--json']);
-
-  const {dependencies} = JSON.parse(huskyVersionDetails);
-
-  if (configFormatShouldBeUpdated(dependencies.husky.version, v3Exists, huskyV4ConfigExists)) {
-    const outdatedConfigMessage = 'Husky configuration is outdated for the installed Husky version';
-
-    info(outdatedConfigMessage, {level: 'secondary'});
-
-    const [results] = await Promise.all([
-      scaffold({projectRoot, packageManager}),
-      v3Exists
-        ? fs.writeFile(packageFilePath, JSON.stringify({
-          ...packageContents,
-          scripts: {...otherScripts}
-        }))
-        : Promise.resolve(),
-      huskyV4ConfigExists ? fs.unlink(`${projectRoot}/.huskyrc.json`) : Promise.resolve()
-    ]);
-
-    return results;
+  if (await configFormatShouldBeUpdated(projectRoot, v3Exists, huskyV4ConfigExists)) {
+    return updateOutdatedConfigFormat(
+      projectRoot,
+      packageManager,
+      v3Exists,
+      packageFilePath,
+      packageContents,
+      otherScripts,
+      huskyV4ConfigExists
+    );
   }
 
   return {};
