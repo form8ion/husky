c30e019acb7d47a57d15a58e93293c0df7fdb369
Matt Travi <programmer@travi.org>
2023-07-14 15:29:15 -0500

    test(unit): adjusted imports of core to account for bundling changes

--- src/hook-creator-test.js
+++ src/hook-creator-test.js
@@ -1,5 +1,5 @@
-import {promises as fs} from 'fs';
-import * as core from '@form8ion/core';
+import {promises as fs} from 'node:fs';
+import core from '@form8ion/core';
 
 import sinon from 'sinon';
 import {assert} from 'chai';
--- src/hook-creator.js
+++ src/hook-creator.js
@@ -1,4 +1,4 @@
-import {promises as fs} from 'fs';
+import {promises as fs} from 'node:fs';
 import {directoryExists} from '@form8ion/core';
 
 export default async function ({configDirectory, hookName, script}) {
