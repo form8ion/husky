0ec776141c0a7cef636433a6a52e53c0ed2a98fe
Matt Travi <programmer@travi.org>
2023-11-28 17:07:57 -0600

    feat(output): logged progress steps for scaffolding

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,11 +1,21 @@
 import makeDir from '../thirdparty-wrappers/make-dir.js';
 import createHook from './hook-creator.js';
+import {info} from '@travi/cli-messages';
+
+async function createPreCommitHook(projectRoot, packageManager) {
+  const configDirectory = await makeDir(`${projectRoot}/.husky`);
+
+  info('Defining pre-commit hook', {level: 'secondary'});
+
+  await createHook({configDirectory, hookName: 'pre-commit', script: `${packageManager} test`});
+}
 
 export default async function ({projectRoot, packageManager, pathWithinParent}) {
   if (pathWithinParent) return {};
 
-  const configDirectory = await makeDir(`${projectRoot}/.husky`);
-  await createHook({configDirectory, hookName: 'pre-commit', script: `${packageManager} test`});
+  info('Scaffolding Husky');
+
+  await createPreCommitHook(projectRoot, packageManager);
 
   return {devDependencies: ['husky@latest'], scripts: {prepare: 'husky install'}};
 }
