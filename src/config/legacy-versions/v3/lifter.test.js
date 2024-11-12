61b5bc148c189816a77a6990e1f919ceb49689cc
Matt Travi <programmer@travi.org>
2024-11-11 22:06:26 -0600

    wip(legacy-config): remove husky npm scripts in the v3 lifter

--- src/config/legacy-versions/v3/lifter.test.js
+++ src/config/legacy-versions/v3/lifter.test.js
@@ -1,3 +1,5 @@
+import {loadPackageJson, writePackageJson} from '@form8ion/javascript-core';
+
 import any from '@travi/any';
 import {describe, vi, expect, it} from 'vitest';
 import {when} from 'jest-when';
@@ -5,6 +7,7 @@ import {when} from 'jest-when';
 import scaffold from '../../../scaffold/scaffolder.js';
 import liftV3Config from './lifter.js';
 
+vi.mock('@form8ion/javascript-core');
 vi.mock('../../../scaffold/scaffolder.js');
 
 describe('v3 config lifter', () => {
@@ -12,8 +15,20 @@ describe('v3 config lifter', () => {
     const projectRoot = any.string();
     const packageManager = any.word();
     const scaffoldResults = any.simpleObject();
+    const otherScripts = any.simpleObject();
+    const otherPackageContents = any.simpleObject();
+    const existingPackageContents = {
+      ...otherPackageContents,
+      scripts: {...otherScripts, precommit: any.string(), commitmsg: any.string()}
+    };
+    when(loadPackageJson).calledWith({projectRoot}).mockResolvedValue(existingPackageContents);
     when(scaffold).calledWith({projectRoot, packageManager}).mockResolvedValue(scaffoldResults);
 
     expect(await liftV3Config({projectRoot, packageManager})).toEqual(scaffoldResults);
+
+    expect(writePackageJson).toHaveBeenCalledWith({
+      projectRoot,
+      config: {scripts: otherScripts, ...otherPackageContents}
+    });
   });
 });
