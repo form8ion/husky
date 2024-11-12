1918dc1abe001c0da4821ebd2dfd0f4b437e0fdc
Matt Travi <programmer@travi.org>
2024-11-11 22:06:26 -0600

    wip(legacy-config): remove husky npm scripts in the v3 lifter

--- src/config/legacy-versions/v3/lifter.js
+++ src/config/legacy-versions/v3/lifter.js
@@ -1,5 +1,16 @@
+import {loadPackageJson, writePackageJson} from '@form8ion/javascript-core';
+
 import scaffold from '../../../scaffold/scaffolder.js';
 
-export default function ({projectRoot, packageManager}) {
+async function removeHuskyNpmScripts(projectRoot) {
+  const packageContents = await loadPackageJson({projectRoot});
+  const {precommit, commitmsg, ...otherScripts} = packageContents.scripts;
+
+  await writePackageJson({projectRoot, config: {...packageContents, scripts: otherScripts}});
+}
+
+export default async function ({projectRoot, packageManager}) {
+  await removeHuskyNpmScripts(projectRoot);
+
   return scaffold({projectRoot, packageManager});
 }
