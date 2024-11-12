515f69a0ff928fe1e40eee3cd5dfcab30a814e38
Matt Travi <programmer@travi.org>
2024-11-11 22:52:38 -0600

    wip(legacy-config): define an overall legacy-config lifter

---
+++ src/config/lifter.test.js
@@ -0,0 +1,27 @@
+import {applyEnhancers} from '@form8ion/core';
+
+import any from '@travi/any';
+import {it, describe, vi, expect} from 'vitest';
+import {when} from 'jest-when';
+
+import * as v3ConfigPlugin from './legacy-versions/v3/index.js';
+import * as v4ConfigPlugin from './legacy-versions/v4/index.js';
+import liftConfig from './lifter.js';
+
+vi.mock('@form8ion/core');
+
+describe('config lifter', () => {
+  it('should lift legacy config formats to the modern format', async () => {
+    const projectRoot = any.string();
+    const packageManager = any.word();
+    const enhancerResults = any.simpleObject();
+    when(applyEnhancers)
+      .calledWith({
+        options: {projectRoot, packageManager},
+        enhancers: {v3Config: v3ConfigPlugin, v4Config: v4ConfigPlugin}
+      })
+      .mockResolvedValue(enhancerResults);
+
+    expect(await liftConfig({projectRoot, packageManager})).toEqual(enhancerResults);
+  });
+});
